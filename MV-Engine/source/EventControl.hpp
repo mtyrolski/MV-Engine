0daa816daa5fdfa333f267609f77f47b0592807a
mvxxx <yohama8832@o2.pl>
2017-01-31 15:14:52 +0100

    extended main, added first body of Event Control

---
+++ MV-Engine/source/EventControl.hpp
@@ -0,0 +1,24 @@
+#pragma once
+
+#include <SFML/Window/Event.hpp>
+
+/*
+				===---===
+	If you want to extend event control you need to modife checkControl() method:
+	-Add new case like this:
+
+	case sf::Event::MouseLeft:
+	{
+		do sth...
+		break;
+	}
+			    ===---===
+*/
+
+namespace mv
+{
+	class EventControl
+	{
+	
+	};
+}
--- MV-Engine/source/main.cpp
+++ MV-Engine/source/main.cpp
@@ -1,10 +1,29 @@
 #include "engine/initializator/Initializator.hpp"
+#include "engine/scene/Scene.hpp"
+#include "engine/loader/Loader.hpp"
 
 int main()
 {
-	mv::Initializator* initializator = new mv::Initializator();
-	initializator->init();
-	delete initializator;
+	mv::Scene* scene;
+
+	{
+		mv::Initializator initializator;
+		initializator.init();
+
+		mv::Loader loader;
+		loader.loadData();
+
+		scene = new mv::Scene(loader.title, sf::Vector2f(loader.ammount.x*loader.cellDimensions.x, loader.ammount.y*loader.cellDimensions.y));
+	}
+
+	while (scene->isOpen())
+	{
+
+	}
+
+}
+	
+	
 
 	return 0;
 }
