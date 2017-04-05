d80a2d91231160dd70c4cb4c6d9150fa502f0daf
mvxxx <yohama8832@o2.pl>
2017-04-05 18:57:18 +0200

    moving camera

--- MV-Engine/source/MV/mouse/Mouse.cpp
+++ MV-Engine/source/MV/mouse/Mouse.cpp
@@ -1,5 +1,5 @@
 #include "Mouse.hpp"
-
+#include <iostream>
 namespace mv
 {
 	void Mouse::draw(sf::RenderTarget & target, sf::RenderStates states) const
@@ -7,8 +7,52 @@ namespace mv
 		target.draw(object, states);
 	}
 
-	Mouse::Mouse(const Mouse::TYPE& type)
+	void Mouse::positionUpdate()
 	{
-		// to do
+		sf::Vector2i pixelPos = sf::Mouse::getPosition(*mv::Scene::getInstance().GetPointerToWindow());
+		sf::Vector2f worldPos = mv::Scene::getInstance().GetPointerToWindow()->mapPixelToCoords(pixelPos);
+		object.setPosition(worldPos);
+	}
+
+	void Mouse::checkBorders()
+	{
+		auto windowDimensions = mv::Scene::getInstance().GetPointerToWindow()->getSize();
+		
+		if (windowDimensions.y - sf::Mouse::getPosition(*mv::Scene::getInstance().GetPointerToWindow()).y < tolerance.y)
+		{
+			mv::Scene::getInstance().moveView(mv::Scene::DIRECTION::DOWN);
+		}
+		else if (sf::Mouse::getPosition(*mv::Scene::getInstance().GetPointerToWindow()).y < tolerance.y)
+		{
+			mv::Scene::getInstance().moveView(mv::Scene::DIRECTION::TOP);
+		}
+
+		if (windowDimensions.x - sf::Mouse::getPosition(*mv::Scene::getInstance().GetPointerToWindow()).x < tolerance.x)
+		{
+			mv::Scene::getInstance().moveView(mv::Scene::DIRECTION::RIGHT);
+		}
+			
+		else if (sf::Mouse::getPosition(*mv::Scene::getInstance().GetPointerToWindow()).x < tolerance.x)
+		{
+			mv::Scene::getInstance().moveView(mv::Scene::DIRECTION::LEFT);
+		}
+
+	}
+
+	Mouse::Mouse(const Mouse::TYPE& type)
+		:tolerance(100, 100)
+	{
+		//texture to do
+	}
+
+	void Mouse::changeType(const Mouse::TYPE & type)
+	{
+		//to do
+	}
+
+	void Mouse::tick()
+	{
+		positionUpdate();
+		checkBorders();
 	}
 }
--- MV-Engine/source/MV/mouse/Mouse.hpp
+++ MV-Engine/source/MV/mouse/Mouse.hpp
@@ -4,6 +4,9 @@
 #include "SFML/Graphics/Sprite.hpp"
 #include "SFML/Graphics/RenderTarget.hpp"
 #include "SFML/Graphics/RenderStates.hpp"
+#include "SFML/Window/Mouse.hpp"
+
+#include "MV/scene/Scene.hpp"
 
 namespace mv
 {
@@ -19,11 +22,20 @@ namespace mv
 	private:
 		sf::Sprite object;
 
+		//to checking border
+		sf::Vector2u tolerance;
 		/* ===Methods=== */
 	public:
 		Mouse(const Mouse::TYPE& type = Mouse::TYPE::DEFAULT);
+
+		void changeType(const Mouse::TYPE& type);
+
+		void tick();
 	protected:
 	private:
 		virtual void draw(sf::RenderTarget& target, sf::RenderStates states) const;
+
+		void positionUpdate();
+		void checkBorders();
 	};
 }
