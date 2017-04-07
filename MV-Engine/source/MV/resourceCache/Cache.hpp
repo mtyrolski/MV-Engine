bc9654a30f5e29d85af8c971f8840096cc66807c
mvxxx <yohama8832@o2.pl>
2017-04-07 19:49:10 +0200

    Repaired cache

--- MV-Engine/source/MV/resourceCache/Cache.hpp
+++ MV-Engine/source/MV/resourceCache/Cache.hpp
@@ -14,11 +14,48 @@ namespace mv
 	public:
 	protected:
 	private:
-		static std::map<std::string, T> resources;
+		std::map<std::string, T> resources;
 		/* ===Methods=== */
 	public:
 		T& get(std::string& path);
+		T& get(std::string&& path);
 	protected:
 	private:
 	};
+
+	template<typename T>
+	inline T & Cache<T>::get(std::string & path)
+	{
+
+		if (path.empty())
+		{
+			Logger::Log("Cache can't find resource in empty path.", Logger::STREAM::BOTH, Logger::TYPE::WARNING);
+		}
+
+		{//Try find resource
+			auto result = resources.find(path);
+			if (result != resources.end())
+				return (T&)result->second;
+		}
+
+		{//Try to load it
+			T resource;
+
+			if (!resource.loadFromFile(path))
+			{
+				Logger::Log("Cache can't find resource in this path.", Logger::STREAM::BOTH, Logger::TYPE::WARNING);
+			}
+
+			resources.emplace(path, resource);
+
+			return (T&)resources[path];
+		}
+	}
+
+	template<typename T>
+	inline T & Cache<T>::get(std::string && path)
+	{
+		return get(path);
+	}
+
 }
